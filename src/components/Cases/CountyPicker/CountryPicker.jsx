7f7723c4b0886783014b05bceb8725f8bf555545
Horuscodes <pipaliya1997@icloud.com>
2021-03-29 22:16:31 -0400

    refactored code

--- src/components/Cases/CountyPicker/CountryPicker.jsx
+++ src/components/Cases/CountyPicker/CountryPicker.jsx
@@ -1,23 +1,10 @@
-import React ,{useState , useEffect} from 'react';
+import React  from 'react';
 import { NativeSelect , FormControl} from '@material-ui/core';
 import styles from '../CountyPicker/CountryPicker.module.css'
-// import { fetchedCountries } from '../../../api'
 
 const CountryPicker = ( {handleCountryChange}) => {
 
-    // const [fetchedCountries , setFetchedCountries] = useState([])
      const fetchedCountries = ['AB' , 'BC', 'MB', 'NB' , 'NL', 'NT', 'NS', 'NU', 'ON', 'PE', 'QC', 'SK','YT','RT']
-
-    // useEffect(()=>{
-    //     // const fetchApi = async () => {
-    //     //     setFetchedCountries(fetchedCountries);
-    //     // }
-
-    //     // fetchApi();
-    //     setFetchedCountries([...fetchedCountries])
-    //     console.log(fetchedCountries)
-    // },[])
-
     return (
         <FormControl className={styles.FormControl}>
             <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
